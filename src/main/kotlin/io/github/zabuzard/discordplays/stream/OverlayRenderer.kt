35600081b3cb2814c2bbe02cc73f7da6e110fc5c
Zabuzard <zabuza.dev@gmail.com>
2023-02-24 10:56:00 +0100

    added server time to overlay

--- src/main/kotlin/io/github/zabuzard/discordplays/stream/OverlayRenderer.kt
+++ src/main/kotlin/io/github/zabuzard/discordplays/stream/OverlayRenderer.kt
@@ -4,11 +4,19 @@ import eu.rekawek.coffeegb.controller.ButtonListener.Button
 import io.github.zabuzard.discordplays.discord.UserInput
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toJavaLocalDateTime
+import kotlinx.datetime.toLocalDateTime
 import me.jakejmattson.discordkt.annotations.Service
+import org.checkerframework.checker.units.qual.g
+import org.intellij.lang.annotations.JdkConstants.FontStyle
 import java.awt.Color
 import java.awt.Font
 import java.awt.Graphics2D
 import java.awt.RenderingHints
+import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.collections.ArrayDeque
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
@@ -38,6 +46,8 @@ class OverlayRenderer {
         limitedHistory.withIndex().forEach { (i, userInput) ->
             userInput.render(g, i, oldBefore)
         }
+
+        renderTimestamp(g, width, height)
     }
 
     private fun UserInput.render(g: Graphics2D, i: Int, oldBefore: Instant) {
@@ -50,6 +60,7 @@ class OverlayRenderer {
         val displayName = user.username.take(NAME_MAX_LENGTH)
 
         g.color = if (sendAt < oldBefore) Color.GRAY else Color.WHITE
+        // Start top left
         val y = HISTORY_OFFSET_Y + HISTORY_ENTRY_PADDING_Y * i
 
         val buttonX =
@@ -57,16 +68,35 @@ class OverlayRenderer {
         g.drawString(buttonLabel, buttonX, y)
         g.drawString(displayName, NAME_OFFSET_X, y)
     }
+
+    private fun renderTimestamp(g: Graphics2D, width: Int, height: Int) {
+        g.color = Color.WHITE
+        g.font = timestampFont
+
+        val now = Clock.System.now().toLocalDateTime(TimeZone.UTC).toJavaLocalDateTime()
+            .let { timestampFormat.format(it) }
+
+        // Bottom right
+        val lineData = g.lineData(now)
+        val x = width - lineData.width - TIMESTAMP_PADDING
+        val y = height - lineData.height + lineData.ascent - TIMESTAMP_PADDING
+
+        g.drawString(now, x, y)
+    }
 }
 
 private const val HISTORY_OFFSET_Y = 20
 private const val HISTORY_ENTRY_PADDING_Y = 25
-private const val HISTORY_MAX_ENTRIES = 14
+private const val HISTORY_MAX_ENTRIES = 13
 private const val BUTTON_LABEL_OFFSET_X = 7
 private const val BUTTON_LABEL_WIDTH = 15
 private const val NAME_OFFSET_X = 35
 private const val NAME_MAX_LENGTH = 15
 private val textFont = Font("Arial", Font.BOLD, 15)
+
+private const val TIMESTAMP_PADDING = 5
+private val timestampFont = Font("Arial", Font.PLAIN, 12)
+private val timestampFormat = DateTimeFormatter.ofPattern("yyy-MM-dd hh:mm:ss", Locale.US)
 
 private val historyEntryOldAfter = (10).seconds
 private val historyEntryExpiresAfter = (2).minutes
